76c0e8b4397524be20480672e834500571908ef6
asafwat <bedo_aboraia@yahoo.com>
2019-10-26 19:37:56 +0200

    Added index.html and style.css

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const formidable = require('express-formidable');
 
 app.use(express.urlencoded({extended: true}));
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use(formidable({
     encoding: 'utf-8',
     uploadDir: __dirname,
     multiples: true,
     keepExtensions: true
-  }));
+}));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
 
 app.post('/api/fileanalyse', (req, res) => {
     var data = req.files.upfile;
